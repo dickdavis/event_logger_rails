5cf9e9cd66752d7e3f3bc698a762aedd12c29397
Dick Davis <dick@hey.com>
2023-09-21 15:14:29 -0500

    Update initializer template for generator

--- lib/generators/event_logger_rails/templates/event_logger_rails.rb
+++ lib/generators/event_logger_rails/templates/event_logger_rails.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-config_file = Rails.root.join('config/event_logger_rails.yml')
-config_from_file = YAML.safe_load(File.read(config_file)).deep_symbolize_keys
+config_file = File.read(Rails.root.join('config/event_logger_rails.yml'))
+config_from_file = YAML.safe_load(config_file).deep_symbolize_keys
 
 EventLoggerRails.setup do |config|
   config.registered_events = config_from_file[:registered_events]
