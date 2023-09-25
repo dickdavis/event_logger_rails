6ac10cfb7e458141b2b9c00a7f2b0451125c026f
Dick Davis <dick@hey.com>
2023-09-24 23:53:06 -0500

    Clean up event logger

--- lib/event_logger_rails/event_logger.rb
+++ lib/event_logger_rails/event_logger.rb
@@ -10,32 +10,31 @@ module EventLoggerRails
   class EventLogger
     def initialize(output_device: $stdout)
       @logger = Logger.new(output_device)
+      @logger.formatter = proc do |severity, datetime, _progname, message|
+        JSON.dump(severity:, timestamp: datetime.to_s, message:)
+      end
     end
 
-    # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
     def log(event, level = :warn, **data)
       event = event.is_a?(EventLoggerRails::Event) ? event : EventLoggerRails::Event.new(event)
       raise EventLoggerRails::Exceptions::UnregisteredEvent.new(unregistered_event: event) unless event.valid?
 
-      logger.formatter = proc do |severity, datetime, _progname, message|
-        JSON.dump(severity:, timestamp: datetime.to_s, message:)
-      end
-
-      begin
-        logger.send(level) do
-          { event_identifier: event.identifier, event_description: event.description }.merge(data)
-        end
-      rescue NoMethodError
-        raise EventLoggerRails::Exceptions::InvalidLoggerLevel.new(logger_level: level)
-      end
+      log_message(event, level, **data)
     rescue EventLoggerRails::Exceptions::UnregisteredEvent,
            EventLoggerRails::Exceptions::InvalidLoggerLevel => error
       log(error.event, :error, message: error.message)
     end
-    # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
     private
 
     attr_reader :logger
+
+    def log_message(event, level, **data)
+      logger.send(level) do
+        { event_identifier: event.identifier, event_description: event.description }.merge(data)
+      end
+    rescue NoMethodError
+      raise EventLoggerRails::Exceptions::InvalidLoggerLevel.new(logger_level: level)
+    end
   end
 end
