b2a9f78617828f4c92b7ccd2eef90d9be2cf9d8e
Dick Davis <dick@hey.com>
2023-09-28 17:33:46 -0500

    Remove initializer from generator

--- lib/generators/event_logger_rails/install_generator.rb
+++ lib/generators/event_logger_rails/install_generator.rb
@@ -7,19 +7,13 @@ module EventLoggerRails
     ##
     # Creates basic config file and initializer for EventLoggerRails
     class InstallGenerator < Rails::Generators::Base
-      desc 'Create basic config file and initializer for EventLoggerRails'
+      desc 'Create event registry file'
       source_root File.expand_path('templates', __dir__)
 
       def copy_config_file
         return if File.exist?(File.join(destination_root, 'config/event_logger_rails.yml'))
 
         copy_file 'event_logger_rails.yml', 'config/event_logger_rails.yml'
-      end
-
-      def copy_initializer
-        return if File.exist?(File.join(destination_root, 'config/initializers/event_logger_rails.rb'))
-
-        copy_file 'event_logger_rails.rb', 'config/initializers/event_logger_rails.rb'
       end
     end
   end
