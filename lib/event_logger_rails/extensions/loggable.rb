2dcb5364a83336be18f87228571c11f430fe20fe
Dick Davis <dick@hey.com>
2023-10-02 21:52:57 -0500

    Clean up loggable

--- lib/event_logger_rails/extensions/loggable.rb
+++ lib/event_logger_rails/extensions/loggable.rb
@@ -6,7 +6,6 @@ module EventLoggerRails
     # Provides event logging with relevant model data.
     module Loggable
       def log_event(event, level: :warn, data: {})
-        logger_trace.disable if instance_variable_defined?(:@logger_call_stack)
         EventLoggerRails.log(event, level, data.merge(optional_data))
       end
 
